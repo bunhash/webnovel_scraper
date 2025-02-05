69b1b69b4e3ac9341f5e62295de477f2034197d3
bunhash <bunhash@bhmail.me>
2025-02-05 10:53:47 -0500

    minor update

--- scraper.ps1
+++ scraper.ps1
@@ -34,7 +34,7 @@ switch -Exact ($Command)
     'crawl' { python $LibDir\crawl.py $args[1..($args.Count)] }
     'download' { python $LibDir\download.py $args[1..($args.Count)] }
     'parse' { python $LibDir\parse_chapters.py $args[1..($args.Count)] }
-    'build' {python $LibDir\make_book.py $args[1..($args.Count)] }
+    'build' { python $LibDir\make_book.py $args[1..($args.Count)] }
     'start' { 
         $cwd = "$(pwd)"
         cd $SrcDir
@@ -53,7 +53,7 @@ switch -Exact ($Command)
         docker compose logs -f
         cd $cwd
     }
-    'rename' {python $LibDir\rename.py $args[1..($args.Count)] }
+    'rename' { python $LibDir\rename.py $args[1..($args.Count)] }
     Default { print_usage }
 }
 
